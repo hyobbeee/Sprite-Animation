e99ef5e7967583b46ef938aee805ec7b52b49924
hyobbeee <yjh5799@dnmd.com>
2022-09-15 13:27:08 +0900

    완료

--- script.js
+++ script.js
@@ -11,19 +11,23 @@ window.addEventListener('load', function () {
             this.image = document.querySelector('#mandrake');
             this.spriteWidth = 256;
             this.spriteHeight = 256;
-            this.width = this.spriteWidth;
-            this.height = this.spriteHeight;
+
+            // this.width = this.spriteWidth;
+            // this.height = this.spriteHeight;
+
             this.scale = 2;
-            this.x = this.canvasWidth / 2 - this.width * this.scale / 2;
-            this.y = this.canvasHeight / 2 - this.height * this.scale / 2;
+            this.x = this.canvasWidth / 2 - this.spriteWidth * this.scale / 2;
+            this.y = this.canvasHeight / 2 - this.spriteHeight * this.scale / 2;
             this.minFrame = 0;
             this.maxFrame = 355;
+
             this.frame = 0;
             this.frameX = 3;
             this.frameY = 0;
         }
+
         draw(context) {
-            context.drawImage(
+            context.drawImage( // drawImage(image, sx, sy, sWidth, sHeight, dx, dy, dWidth, dHeight)
                 this.image,
                 this.frameX * this.spriteWidth,
                 this.frameY * this.spriteHeight,
@@ -31,8 +35,8 @@ window.addEventListener('load', function () {
                 this.spriteHeight,
                 this.x,
                 this.y,
-                this.width * this.scale,
-                this.height * this.scale,
+                this.spriteWidth * this.scale,
+                this.spriteHeight * this.scale,
             );
         }
 
@@ -55,17 +59,26 @@ window.addEventListener('load', function () {
     }
 
     const mandrake = new Mandrake(canvas.width, canvas.height);
+    console.log(mandrake.__proto__);
+    console.log(Mandrake.prototype);
+    console.log(mandrake.constructor);
+
+    // new 연산자
 
     function animate() {
         ctx.clearRect(0, 0, canvas.width, canvas.height);  //canvas의 뿌려진 부분 초기화  
         mandrake.draw(ctx);
         mandrake.update();
         requestAnimationFrame(animate);
+        //requestAnimationFrame(반복할 함수)
+        // - 백그라운드 동작 및 비활성화시 중지(성능 최적화)
+        // - 최대 1ms(1/1000s)로 제한되며 1초에 60번 동작
+        // - 다수의 애니메이션에도 각각 타이머 값을 생성 및 참조하지 않고 내부의 동일한 타이머 참조 
     }
     animate();
 
-    //애니메이션 버튼
 
+    //애니메이션 버튼
     const all = document.querySelector('#all')
     all.addEventListener('click', function () {
         mandrake.setAnimation(0, 355);
